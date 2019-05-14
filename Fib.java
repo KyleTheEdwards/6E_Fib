0c469f1142bf921c5201f25ce0fd9ede443ff5f8
KillyBOT <edwards.kyle.j@gmail.com>
2019-05-13 20:34:03 -0400

    Finished skeleton

--- Fib.java
+++ Fib.java
@@ -1,3 +1,5 @@
+import java.lang.Math;
+
 /** 
   Calculate Fibonacci number n
   3 implementations
@@ -11,10 +13,11 @@ public class Fib {
       @return the nth Fibonacci number
               by implementing the recurrence relation
      */
-    public ?? long fib_recurrence( int n) {
-        return -2; // invalid Fibonacci number
+    public static long fib_recurrence( int n) {
+      if(n == 2 || n == 1) return 1;
+      else return fib_recurrence(n-1) + fib_recurrence(n-2);
     }
-     /* These are class / instance methods because...
+     /* These are class methods because they do not require any other class 
     */
 
 
@@ -22,18 +25,27 @@ public class Fib {
       @return the nth Fibonacci number
               calculated via the 8th-grade algorithm
      */
-    public ?? long fib_grade8( int n) {
-        return -2; // invalid Fibonacci number
+    public static long fib_grade8( int n) {
+      long nthFib = 0;
+      long afterNthFib = 1;
+      long temp;
+      for(int currentFibNum = 0; currentFibNum < n; currentFibNum++){
+        temp = nthFib;
+        nthFib = afterNthFib;
+        afterNthFib += temp;
+      }
+
+      return nthFib;
     }
     /* Time complexity:
        Consider the size of the problem to be ...
        
-       As the proxy for the time required, count...
+       As the proxy for the time required, count the amount of additions done
        
        Then cost of the the recurrence algorithm
-       grows ?? 
+       grows linearlly
        as the size of the problem increases,
-       because ??
+       because to find the nth fibonacci number, we must add n numbers
      */
 
 
@@ -43,17 +55,17 @@ public class Fib {
               Type is double so that this exercise can
               ignore rounding issues.
      */
-    public ?? double fib_Binet( int n) {
-        return -2; // invalid Fibonacci number
+    public static double fib_Binet( int n) {
+        return (Math.pow((1 + Math.sqrt(5))/2,n) - Math.pow((1 - Math.sqrt(5))/2,n)) / Math.sqrt(5);
     }
     /* Time complexity:
-       Consider the size of the problem to be ...
+       Consider the size of the problem to be the 
        
-       As the proxy for the time required, count...
+       As the proxy for the time required, count the amount of operations done in the formula
        
        Then cost of the the recurrence algorithm
-       grows ?? 
+       grows at a linear rate
        as the size of the problem increases,
-       because ??
+       because aan exponential multiplication is linear, since it requires multiplying a number by itself a certain amount of times.
      */
 }
